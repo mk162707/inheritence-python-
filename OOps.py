b918d214559bf58d19d54ef816018a1c71b7f4ee
mk162707 <maanmk58@gmail.com>
2024-07-18 11:38:27 +0530

    Add files via upload

---
+++ OOps.py
@@ -0,0 +1,25 @@
+'''
+
+ object oriented progarmming lang:
+ 1) object
+ 2) class
+ 3) encapsulation + data binding  +  { security }
+ 4) abstractiobn  + data hiding    +
+ 5) inheritence  => reuse 
+ 6) polymorphism  => different forms => (extended version / more explaied version of inherie)
+ hidden concepts :
+ 1) constructors + most imp 
+
+'''
+
+class demo:
+    a=10
+
+
+# driver code
+obj =demo()
+print(obj.a)
+
+# Objct_name =classname()
+# print(onjectName. variables)
+
